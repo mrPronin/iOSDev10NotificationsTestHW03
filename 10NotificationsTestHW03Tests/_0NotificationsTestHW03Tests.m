d1b8a62496ede3bf5d97b16480767417aa30dfe1
Alexander Pronin <pronin.alx@gmail.com>
2014-02-07 10:17:57 +0200

    Initial Commit

---
+++ 10NotificationsTestHW03Tests/_0NotificationsTestHW03Tests.m
@@ -0,0 +1,34 @@
+//
+//  _0NotificationsTestHW03Tests.m
+//  10NotificationsTestHW03Tests
+//
+//  Created by Pronin Alexander on 07.02.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0NotificationsTestHW03Tests : XCTestCase
+
+@end
+
+@implementation _0NotificationsTestHW03Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
